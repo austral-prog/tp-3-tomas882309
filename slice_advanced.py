c788731702a03c8b07f624dcd9f76cd07b664d6a
Tomas Scherer <141924902+tomas882309@users.noreply.github.com>
2024-04-08 22:42:22 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -3,7 +3,7 @@ def slice_advanced():
     
     texto = input ("ingrese un texto:")
 
-    print (texto [4: :2])
+    print (texto [4::2])
     
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
